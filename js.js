d34b7fff6ada5f3bc55676bea4571d0435433e9e
Seb <gillisl22@outlook.com>
2022-07-28 15:20:19 -0400

    adding the 5 days at the bottom

--- js.js
+++ js.js
@@ -6,13 +6,6 @@ var day4 = $("#day4");
 var day5 = $("#day5");
 
 var apiKey = '44541d76d19b2706ab8b90c24f5bbf53';
-var weatherMain = document.getElementById("weather1");
-var tempMain = document.getElementById("temp1");
-var windMain = document.getElementById("wind1");
-var humidityMain = document.getElementById("humidity1");
-var uvMain = document.getElementById("uv1");
-var lat = "";
-var lon = "";
 
 function cityandtime() {
     var timeNow = moment().format('(M/D/YYYY)'); 
@@ -29,6 +22,24 @@ function cityandtime() {
     day5.text(timeNow);
 }
 
+// preloaded city
+function preloading() {
+    var city = "toronto";
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=44541d76d19b2706ab8b90c24f5bbf53`
+    )
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                localStorage.setItem("city", JSON.stringify(data))
+            })
+        }
+    })
+    cityandtime()
+    loadWeather()
+    loadWeatherFive()
+};
+
 // weather info for the choosen city
 function searching() {
     var searchInput = document.getElementById('search-input');
@@ -44,16 +55,31 @@ function searching() {
             })
         }
     })
-    loadWeather()
+        loadWeather()
+        loadWeatherFive()
 };
 
+// actively loading
 function loadWeather() {
     const cityLocal = JSON.parse(localStorage.getItem("city"));
-    document.getElementById("weather1").textContent = cityLocal.weather.icon;
-    document.getElementById("temp1").textContent = cityLocal.main.temp;
-    document.getElementById("wind1").textContent = cityLocal.wind.speed;
-    document.getElementById("humidity1").textContent = cityLocal.main.humidity;
-    document.getElementById("uv1").textContent = cityLocal.wind.speed;
+    let icon = cityLocal.weather[0].icon;
+    document.getElementById("Location").textContent = cityLocal.name;
+    document.getElementById("weatherLarge").innerHTML = `<img src=https://openweathermap.org/img/wn/${icon}@4x.png>`;
+    document.getElementById("tempLarge").textContent = ("temperature " + cityLocal.main.temp + "C");
+    document.getElementById("windLarge").textContent = ("wind speeds " + cityLocal.wind.speed + "KM/h");
+    document.getElementById("humidityLarge").textContent = ("Humidity " + cityLocal.main.humidity + "%");
 }
 
-window.onload = cityandtime;
+// five bottom displays
+function loadWeatherFive() {
+    const cityLocal = JSON.parse(localStorage.getItem("city"));
+    for (x = 0; x < 5 ; x++) {
+        let icon = cityLocal.weather[0].icon;
+        document.getElementById(`${x}weather`).innerHTML = `<img src=https://openweathermap.org/img/wn/${icon}@4x.png>`;
+        document.getElementById(`${x}temp`).textContent = ("temperature " + cityLocal.main.temp + "C");
+        document.getElementById(`${x}wind`).textContent = ("wind speeds " + cityLocal.wind.speed + "KM/h");
+        document.getElementById(`${x}hum`).textContent = ("Humidity " + cityLocal.main.humidity + "%");
+       }
+    };
+
+window.onload = preloading;
